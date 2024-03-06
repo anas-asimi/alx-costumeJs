e68f93cd907477ad9c06e629efe550741d457300
Anas Asimi <anas2020cr7@gmail.com>
2024-03-06 14:28:21 +0100

    improve

--- main.js
+++ main.js
@@ -53,6 +53,7 @@ if (isQA_available() == false) {
 /*
 * add "check all tasks" button
 */
+console.log('add "check all tasks" button');
 const myBtn = document.createElement("button")
 myBtn.textContent = "check all tasks"
 myBtn.className = "btn btn-primary"
@@ -67,11 +68,15 @@ document.body.appendChild(myBtn)
 /*
 * add the remaining hours
 */
+console.log('add the remaining hours');
 const projectMetadata = document.getElementById('project-metadata')
 const projectCalendar = projectMetadata.querySelector('.fa-calendar').parentElement
 const projectEndDate = [...projectCalendar.querySelectorAll('.datetime')].pop()
 let hours = (new Date(projectEndDate.textContent) - new Date()) / 1000 / 60 / 60
-projectEndDate.parentElement.innerHTML += `<span style="font-weight: 600;>(${hours.toFixed(0)} hours)</span>`
+const hoursElement = document.createElement("span")
+hoursElement.textContent = `  (${hours.toFixed(0)} hours)`
+hoursElement.style.fontWeight = 600
+projectCalendar.appendChild(hoursElement)
 // =======================================================================>
 /*
 * wait for element to render
