0619a39a28191b283388111fc5d8d1c53245ac8b
Anas Asimi <anas2020cr7@gmail.com>
2024-08-10 17:25:42 +0100

    fix Filename extension

--- main.js
+++ main.js
@@ -3,8 +3,9 @@
 console.log("starting the script v2.0");
 
 const PATH = 'https://anas-asimi.github.io/alx-costumeJs/'
+const EXTENSION = '.js'
 const filesNames = ['remainingHours', 'IndexingQA']
 
 for (const fileName of filesNames) {
-  import(PATH + fileName + 'js')
+  import(PATH + fileName + EXTENSION)
 }
